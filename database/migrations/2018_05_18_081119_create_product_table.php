606ca09e03373fc838a82b6d8d661ec425f4a8f0
nguyenxuanphuong1211 <nguyenxuanphuong1211@gmail.com>
2018-05-21 10:21:19 +0700

    add_image_product

--- database/migrations/2018_05_18_081119_create_product_table.php
+++ database/migrations/2018_05_18_081119_create_product_table.php
@@ -16,6 +16,7 @@ class CreateProductTable extends Migration
         Schema::create('Products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('image');
             $table->longText('description_detail');
             $table->longText('description_brief');
             $table->enum('hot',['1','0']);
